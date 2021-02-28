d5570cd947beaeb7d6fb3574e9d7512ba6c8373c
nancyaprelia24 <64649384+nancyaprelia24@users.noreply.github.com>
2021-02-28 15:02:12 +0700

    AddDatabase

---
+++ mahasiswa.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 5.5.5-10.1.38-MariaDB : Database - mahasiswa
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mahasiswa` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mahasiswa`;
+
+/*Table structure for table `mahasiswa` */
+
+DROP TABLE IF EXISTS `mahasiswa`;
+
+CREATE TABLE `mahasiswa` (
+  `nama` varchar(225) NOT NULL,
+  `npm` int(20) NOT NULL,
+  `jurusan` varchar(225) DEFAULT NULL,
+  `alamat` varchar(225) DEFAULT NULL,
+  `phone` int(20) DEFAULT NULL,
+  PRIMARY KEY (`npm`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `mahasiswa` */
+
+insert  into `mahasiswa`(`nama`,`npm`,`jurusan`,`alamat`,`phone`) values ('Nancy',11318046,'Teknik Informatika','Laguboti',98789);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
